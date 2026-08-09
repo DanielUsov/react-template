d0617845d872cafbe54e967ff8883364782e8b34
Daniel <danielusovdev@gmail.com>
2026-08-09 13:24:52 +0300

    feat: init

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,4 @@
 import js from '@eslint/js';
-import pluginQuery from '@tanstack/eslint-plugin-query';
-import tsParser from '@typescript-eslint/parser';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import boundaries from 'eslint-plugin-boundaries';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -14,18 +7,7 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: {
-      boundaries
-    },
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      eslintConfigPrettier,
-      ...pluginQuery.configs['flat/recommended'],
-      eslintPluginPrettier
-    ],
+    extends: [js.configs.recommended, tseslint.configs.strictTypeChecked],
     rules: {
       'boundaries/element-types': [
         'error',
@@ -35,8 +17,15 @@ export default defineConfig([
       ]
     },
     languageOptions: {
-      parser: tsParser,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        projectService: true
+      },
       globals: globals.browser
     }
+  },
+  {
+    files: ['**/*.js'],
+    extends: [js.configs.recommended, tseslint.configs.disableTypeChecked]
   }
 ]);
